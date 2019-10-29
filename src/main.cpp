1fb5e393161dce3d1004b6207eb6109d8c324dc0
lutrarutra <artur.gynter@hotmail.com>
2019-10-29 13:57:26 +0200

    SFML window test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
+#include "SFML/Graphics.hpp"
+
 int main()
 {
-    std::cout << "Hello, Traffic sim!" << "\n";
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Traffic simulator");
+
+    while (window.isOpen())
+    {
+        sf::Event Event;
+        while (window.pollEvent(Event))
+        {
+            if (Event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
+    std::cout << "Hello, Traffic sim!"<< "\n";
 }
